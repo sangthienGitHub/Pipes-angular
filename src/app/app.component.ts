1ab40aa98131ffd1b5ebb4a80e0ef4978eb116b8
sangTran <sangthien2010@gmail.com>
2020-04-14 22:58:38 -0500

    adding bootstrap and form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string;
+  date: string;
+  amount: number;
+  height: number;
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000,
+  };
+
+  onHeightChange(value: string) {
+    this.height = parseFloat(value);
+  }
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
 }
